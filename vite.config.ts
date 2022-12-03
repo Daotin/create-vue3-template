3bb72075d5c614cbf16c4b68215bcaa895ca896f
Daotin <daotin@qq.com>
2022-12-03 15:55:04 +0800

    feat(其他): 修改vite相关配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      // fileURLToPath:函数确保百分比编码字符的正确解码，并确保跨平台的有效绝对路径字符串。
+      // URL:如果url参数是相对 URL，则构造函数将使用url参数和可选的base参数作为基础
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
