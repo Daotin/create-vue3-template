4f1286df65d2ed93930adbc80e87a14f4f3c5e42
Daotin <daotin@qq.com>
2022-12-03 17:06:16 +0800

    feat(其他): 增加ElementPlus组件库，并设置为生产环境按需加载，开发环境全量加载

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -14,6 +20,8 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+
+      <el-button type="primary">按钮</el-button>
     </div>
   </header>
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,46 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
+// Element Plus按需引入
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+// 解决ElementPlus非标签元素丢失样式的问题
+import ElementPlus from "unplugin-element-plus/vite";
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      // fileURLToPath:函数确保百分比编码字符的正确解码，并确保跨平台的有效绝对路径字符串。
-      // URL:如果url参数是相对 URL，则构造函数将使用url参数和可选的base参数作为基础
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        // 生产环境按需导入
+        resolvers: mode !== "development" ? ElementPlusResolver() : undefined,
+      }),
+      // 开发环境完整引入element-plus
+      {
+        name: "dev-auto-import",
+        transform(code, id) {
+          if (mode === "development" && /src\/main.ts$/.test(id)) {
+            return {
+              code: `${code};import ElementPlus from 'element-plus';import 'element-plus/dist/index.css';app.use(ElementPlus);`,
+              map: null,
+            };
+          }
+        },
+      },
+      ElementPlus(),
+    ],
+    resolve: {
+      alias: {
+        // fileURLToPath:函数确保百分比编码字符的正确解码，并确保跨平台的有效绝对路径字符串。
+        // URL:如果url参数是相对 URL，则构造函数将使用url参数和可选的base参数作为基础
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+  };
 });
