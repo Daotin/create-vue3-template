19ee10e07c398d5b471b4686ee20e1fa2e19664e
冯道廷 <fengdaoting@whty.com.cn>
2022-12-08 10:17:48 +0800

    feat(styles): 新增styles样式文件

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import App from './App.vue'
 import router from './router'
 import pinia from './stores'
 
-import './assets/main.less'
+import './assets/styles/main.less'
 // import './mocks'
 
 window.$message = message
