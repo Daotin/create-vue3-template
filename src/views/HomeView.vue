c6b320328d11111617446bd1943ba6de72981ea5
Daotin <daotin@qq.com>
2025-03-11 21:12:28 +0800

    docs: 增加cursor rules规范

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,6 +8,7 @@ console.log('⭐==>', enums.common.enumState.enabled)
 	<main>Home</main>
 	<el-button type="primary" class="mt-4" v-auth="'home:button'">Home按钮</el-button>
 	<icon-ep-edit />
+	<!-- <i-ep-edit /> 不能显示的原因是unplugin-icons默认使用i-作为前缀，但项目配置了prefix: 'icon'，所以只有<icon-ep-edit />能正常工作。 -->
 	<icon-ep-delete class="custom-icon" style="color: red; font-size: 24px" />
 
 	<BaseIcon name="avatar" size="24" />
